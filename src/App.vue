bea042d66b0aa81ab2fe4d305291e940d56ce825
william ali <william.ali@hotmail.se>
2022-08-29 17:47:33 +0200

    Re-wrote fetch api call style changes

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,70 @@
 <template>
-<router-view/>
-<video autoplay loop muted>
-  <source src="./assets/AnimatedBackground/Test.mp4" width="1920px">
-</video>
+  <router-view />
+  <video autoplay loop muted>
+    <source src="./assets/AnimatedBackground/Test.mp4">
+  </video>
 </template>
 
 <style>
-
-html {
+html
+{
   overflow: hidden;
 }
-body {
-  margin:0;
+
+body
+{
+  margin: 0;
 }
-#app {
+
+#app
+{
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  font-family:cursive;
+  font-family: cursive;
 }
-source {
+
+source
+{
+  width: 1920px;
   object-fit: fill;
 }
-nav {
+
+nav
+{
   padding: 30px;
 }
 
-nav a {
+nav a
+{
   font-weight: bold;
   color: #2c3e50;
 }
 
-nav a.router-link-exact-active {
+nav a.router-link-exact-active
+{
   color: #42b983;
 }
 
-@media (max-width: 425px) {
-  source {
-  object-fit: fill;
-}  
+@media (max-width: 425px)
+{
+
+  html {
+    overflow: hidden;
+  }
+
+  source
+  {
+    width: 100%;
+  }
+
+  video
+  {
+    width: 100%;
+    height: 900px;
+    object-fit: cover;
+  }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,27 @@
 <template>
-  <div class="wrapper">
-    <input class="input" v-on:keydown.enter="fetchWeather()" v-model="query" type="text"
-      placeholder="var vill du veta vädret?" />
-    <div v-if="query.length > 1">
-      <h1> <span style="text-transform:uppercase">{{ country }} {{ date }}</span></h1>
-      <h1 class="temp">{{ temp }} °C</h1>
-      <h1>{{ conditions }}</h1>
-    </div>
+  <div class="container">
+    <transition name="fade">
+      <div class="wrapper">
+        <input id="input-field" class="input" v-on:keydown.enter="fetchWeather()" v-model="query" type="text"
+          placeholder="var vill du veta vädret?" required />
+        <transition name="fade">
+          <div class="wheater-info-container" v-if="wheaterFetched">
+            <h1> <span style="text-transform:uppercase">{{  wheaterData.address  }} {{  currentTime  }}</span></h1>
+            <h1 class="temp">{{  wheaterData.currentConditions.temp  }} °C</h1>
+            <h1>{{  wheaterData.currentConditions.conditions  }}</h1>
+            <div class="days-container">
+              <section class="days" v-for="(days, index) in wheaterData.days.slice(0, 7)" :key="index">
+                <h3>{{  getDayOfWeek(days.datetime)  }}</h3>
+                <h4>{{  days.temp  }} °C</h4>
+              </section>
+            </div>
+          </div>
+          <div v-else-if="notFound">
+            <h1>Staden du sökte efter finns ej, försök igen!</h1>
+          </div>
+        </transition>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -15,60 +30,94 @@ export default {
   name: 'HomeView',
   data() {
     return {
-      country: '',
-      conditions: '',
-      temp: '',
+      wheaterData: [],
+      currentTime: '',
+      wheaterFetched: false,
+      notFound: false,
       query: '',
-      date: ''
+      errorMsg: 'staden du sökte efter finns ej, försök igen!'
     }
   },
   methods: {
     fetchWeather() {
-      fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + this.query + "?unitGroup=metric&include=current&key=HJ6JGEJRS4S4MZ7XBLDFDAZA2&contentType=json", {
-        "method": "GET",
-        "headers": {
-        }
-      })
+      fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
+        + this.query +
+        "?unitGroup=metric&include=current&key=HJ6JGEJRS4S4MZ7XBLDFDAZA2&contentType=json",
+        {
+          "method": "GET",
+          "headers": {
+          }
+        })
         .then(response => response.json())
         .then(data => {
-          this.country = data.address
-          this.temp = data.currentConditions.temp
-          this.conditions = data.currentConditions.conditions
-          this.date = data.currentConditions.datetime
-          console.log(data)
+          this.wheaterData = data;
+          this.notFound = false;
+          this.wheaterFetched = true;
+          this.currentTime = new Date().toLocaleTimeString();
+          this.query = '';
+          console.log(this.wheaterData)
         })
         .catch(err => {
+          this.notFound = true;
+          this.wheaterFetched = false;
+          this.query = '';
           console.error(err);
         });
+    },
+    getDayOfWeek(date) {
+      const dayOfWeek = new Date(date).getDay();
+      return isNaN(dayOfWeek) ? null :
+        ['Söndag', 'Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag'][dayOfWeek];
     }
   }
 }
 </script>
 <style scoped>
-.input
+.container
 {
-  border-radius: 10px 0px 10px 0px;
-  padding: 10px;
-  background: rgba(0, 0, 0, 0.541);
-  font-size: 16px;
-  color: white;
-  text-shadow: 2px 1.4px black;
-}
-
-h1
-{
-  color: black;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 800px;
 }
 
 .wrapper
 {
-  padding: 30px;
-  background: rgba(31, 18, 18, 0.342);
-  border-radius: 15px;
-  z-index: 100;
+  display: flex;
+  flex-direction: column;
+  height: 525px;
   position: absolute;
-  top: 30%;
-  left: 45%;
+  z-index: 100;
+  transition: 2s;
+}
+
+.input
+{
+  border-radius: 10px 0px 10px 0px;
+  padding: 10px;
+  margin-bottom: 10px;
+  background: rgba(0, 0, 0, 0.540);
+  font-size: 16px;
+  color: white;
+  text-shadow: 2px 1.4px black;
+  font-weight: bold;
+  text-align: center;
+  text-transform: uppercase;
+}
+
+.input::placeholder
+{
+  color: lightgray;
+}
+
+
+h1,
+h3,
+h4
+{
+  color: white;
 }
 
 .temp
@@ -76,7 +125,7 @@ h1
   display: flex;
   justify-content: center;
   align-items: center;
-  border: 2px solid gray;
+  border: 2px solid white;
   box-shadow: 3px 3px 3px 50px inset rgba(0, 0, 0, 0.541);
   border-radius: 10px;
   width: 100%;
@@ -85,16 +134,55 @@ h1
   font-size: 40px;
 }
 
+.wheater-info-container
+{
+  background: rgba(31, 18, 18, 0.600);
+  border: 2px solid white;
+  border-radius: 15px;
+  padding: 30px;
+
+}
+
+.days-container
+{
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  column-gap: 20px;
+  row-gap: 20px;
+  flex-wrap: wrap;
+  border-radius: 10px;
+}
+
+.days
+{
+  border: 2px solid white;
+  border-radius: 8px;
+  padding: 10px;
+}
+
+.fade-enter-active,
+.fade-leave-active
+{
+  transition: opacity 1.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to
+{
+  opacity: 0;
+}
+
+
 @media (max-width: 425px)
 {
-  .wrapper
-  {
-      display:flex;
-  flex-direction: column;
-  gap: 100px;
-    left: 57%;
-    height: 60%;
-    width: 100%;
+
+  .input {
+    width: 95%;
+  }
+  .days {
+    font-size: 12px;
   }
 }
 </style>
